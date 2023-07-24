97e8482fb03ccaf9cc47ac7f4a085deb498eed09
DerickT02 <derickt18@outlook.com>
2023-07-24 08:37:45 -0700

    Create blog will now route with an id that corresponds to the created document in firebase

--- src/app/firebase/queries.ts
+++ src/app/firebase/queries.ts
@@ -1,5 +1,6 @@
-import { getFirestore } from 'firebase/firestore'
-
+import { getFirestore, setDoc, doc, addDoc, collection } from 'firebase/firestore'
+import { app } from './firebase'
+import Router from "next/router";
 /*
     Blog structure
 
@@ -8,4 +9,35 @@ import { getFirestore } from 'firebase/firestore'
     textContent: [Array]
     comments: [Array]
 */
+
+const db = getFirestore(app)
+
+
+export async function postBlog(){
+    
+   let result = await addDoc(collection(db, "Blogs"), {
+        title: "",
+        content: "",
+    })
+    
+   return result
+    
+}
+
+export async function updateBlog(id: string, title: string, content: string){
+    const docRef = doc(db, "Blogs", id)
+    await setDoc(docRef, {
+        title: title,
+        content: content
+    }, {merge: true})
+    .then((res) => {
+        console.log("Blog added")
+        console.log(res)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+    
+}
+
 
